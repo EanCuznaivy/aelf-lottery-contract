123dc3546d4732057a5cd1d7cf475c4123289ec6
yiqi <yiqi.zhao@aelf.io>
2020-05-12 22:29:04 +0800

    Fix some refs.

--- chain/src/AElf.Boilerplate.Tester/TesterModule.cs
+++ chain/src/AElf.Boilerplate.Tester/TesterModule.cs
@@ -9,7 +9,6 @@ namespace AElf.Boilerplate.Tester
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            context.Services.AddSingleton<ISystemTransactionGenerator, LotteryTransactionGenerator>();
         }
     }
 }
